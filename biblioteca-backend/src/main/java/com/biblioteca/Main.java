d1d815261d3482dc3d2ea5a29bde77ebe7fae5ec
pjg09 <pedroj1229@gmail.com>
2026-02-13 15:36:58 -0500

    SetUp del proyecto en maven para gestionar dependencias y que la vida sea mas bonita, adicionalmente se cargaron las dos clases base que ya estaban creadas de las cuales se parte

---
+++ biblioteca-backend/src/main/java/com/biblioteca/Main.java
@@ -0,0 +1,13 @@
+package com.biblioteca;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println(""); // Dejar este espacio en blanco al comienzo para que se vea mejor en consola
+
+        System.out.println(""); // // Dejar este espacio en blanco al final para que se vea mejor en consola
+
+    }
+    
+}
